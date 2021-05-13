98baec658f9732701fda36b5be5a91d8177eb23d
Pavel Borčin <xborcin1@fi.muni.cz>
2021-05-13 12:46:48 +0200

    first round

--- BufferOverflow.cpp
+++ BufferOverflow.cpp
@@ -9,17 +9,18 @@
 using namespace std;
 // Note: GCC and MSVC uses different memory alignment
 // Try "12345678DevilEvecosia" as a password for gcc build
-// Try "1234567812345678Devil I am. Ha Ha" as a password for MSVC debug x86 build. 
+// Try "1234567812345678Devil I am. Ha Ha" as a password for MSVC debug x86 build.
 // If doesn't work, investigate why :)
 
-void demoBufferOverflowData() {
-	int             unused_variable = 30;
-#define NORMAL_USER            'n'
-#define ADMIN_USER             'a'
-	int             userRights = NORMAL_USER;
-#define USER_INPUT_MAX_LENGTH  8
-	char    userName[USER_INPUT_MAX_LENGTH];
-	char    passwd[USER_INPUT_MAX_LENGTH];
+void demoBufferOverflowData()
+{
+	int unused_variable = 30;
+#define NORMAL_USER 'n'
+#define ADMIN_USER 'a'
+	int userRights = NORMAL_USER;
+#define USER_INPUT_MAX_LENGTH 8
+	char userName[USER_INPUT_MAX_LENGTH];
+	char passwd[USER_INPUT_MAX_LENGTH];
 
 	// print some info about variables
 	printf("%-20s: %p\n", "userName", userName);
@@ -41,15 +42,17 @@ void demoBufferOverflowData() {
 	// Get password
 	printf("%s@vulnerable.machine.com: ", userName);
 	fflush(stdout);
-	//gets(passwd);  
+	//gets(passwd);
 	scanf("%s", passwd); // use scanf("%s", passwd); if gets fails with identifier not found
 
 	// Check user rights (set to NORMAL_USER and not changed in code)
-	if (userRights == NORMAL_USER) {
+	if (userRights == NORMAL_USER)
+	{
 		printf("\nWelcome, normal user '%s', your rights are limited.\n\n", userName);
 		fflush(stdout);
 	}
-	if (userRights == ADMIN_USER) {
+	if (userRights == ADMIN_USER)
+	{
 		printf("\nWelcome, all mighty admin user '%s'!\n", userName);
 		fflush(stdout);
 	}
@@ -62,8 +65,8 @@ void demoBufferOverflowData() {
 	//fgets(passwd, USER_INPUT_MAX_LENGTH - 1, stdin);
 }
 
-
-void demoAdjacentMemoryOverflow(char* userName, char* password) {
+void demoAdjacentMemoryOverflow(char *userName, char *password)
+{
 	// See more at http://www.awarenetwork.org/etc/alpha/?x=5
 	// Once string is not null terminated, a lot of functions will behave wrongly:
 	// sprintf, fprintf, snprintf, strcpy, strcat, strlen, strstr, strchr, read...
@@ -83,45 +86,51 @@ void demoAdjacentMemoryOverflow(char* userName, char* password) {
 
 	memset(buf, 0, sizeof(buf));
 	memset(message, 1, sizeof(message));
-	strncpy(buf, userName, sizeof(buf));              // We will copy only characters which fits into buf
+	strncpy(buf, userName, sizeof(buf)); // We will copy only characters which fits into buf
 
-													  // Now print username to standard output - nothing sensitive, right?
+	// Now print username to standard output - nothing sensitive, right?
 	sprintf(message, "Checking '%s' password\n", buf);
 	printf("%s", message);
-	if (strcmp(password, realPassword) == 0) {
+	if (strcmp(password, realPassword) == 0)
+	{
 		printf("Correct password.\n");
 	}
-	else {
+	else
+	{
 		printf("Wrong password.\n");
 	}
 
-	// FIX: Do not allow to have non-terminated string 
+	// FIX: Do not allow to have non-terminated string
 	// Clear buffer for text with zeroes (terminating zero will be there)
 	// strncpy(buf,arg1,sizeof(buf) - 1);
 }
 
-typedef struct _some_structure {
-	float   someData[1000];
+typedef struct _some_structure
+{
+	float someData[1000];
 } some_structure;
 
-void demoDataTypeOverflow(int totalItemsCount, some_structure* pItem, int itemPosition) {
+void demoDataTypeOverflow(int totalItemsCount, some_structure *pItem, int itemPosition)
+{
 	// See http://blogs.msdn.com/oldnewthing/archive/2004/01/29/64389.aspx
-	some_structure* data_copy = NULL;
+	some_structure *data_copy = NULL;
 	int bytesToAllocation = totalItemsCount * sizeof(some_structure);
 	printf("Bytes to allocation: %d\n", bytesToAllocation);
-	data_copy = (some_structure*)malloc(bytesToAllocation);
-	if (itemPosition >= 0 && itemPosition < totalItemsCount) {
+	data_copy = (some_structure *)malloc(bytesToAllocation);
+	if (itemPosition >= 0 && itemPosition < totalItemsCount)
+	{
 		memcpy(&(data_copy[itemPosition]), pItem, sizeof(some_structure));
 	}
-	else {
+	else
+	{
 		printf("Out of bound assignment");
 		return;
 	}
 	free(data_copy);
 }
 
-
-int main() {
+int main()
+{
 	printf("%-20s: %p\n", "main", main);
 	printf("\n");
 
@@ -153,7 +162,7 @@ int main() {
 
 	//
 	// Not only arrays may overflow - integers as well
-	//  
+	//
 	//
 	/*
 	// printf("\n\n#### demoDataTypeOverflow ####\n");
@@ -165,6 +174,3 @@ int main() {
 
 	return 0;
 }
-
-
-
